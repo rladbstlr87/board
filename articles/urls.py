d21d97d917086535c91bbe120ba64dfee405b232
rladbstlr87 <manylong@naver.com>
2025-03-20 14:45:43 +0900

    delete 기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,14 @@ from . import views
 app_name = 'articles' # 다른 앱에도 detail같이 변수 같은 경우 발생할 수 있어서 정해주는 역할
 
 urlpatterns = [
+    # Read
     path('', views.index, name='index'), # 이 경로를 'index'로 변수화
     path('<int:id>/', views.detail, name='detail'), # `name=`이 인스턴스화 역할 함
 
+    # Create
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+
+    # Delete
+    path('<int:id>/delete/', views.delete, name='delete'),
 ]
