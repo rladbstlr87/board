dc97fe11e8a7bc2815f31cb6f686b29e6b393834
rladbstlr87 <manylong@naver.com>
2025-03-26 17:07:27 +0900

    login 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def signup(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
